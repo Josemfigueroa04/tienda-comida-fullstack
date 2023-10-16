829f641ea324d2acd9620d8267f1cecb6996daf9
josemfiguero04 <josemfigueroa04@gmail.com>
2023-10-16 02:37:02 -0300

    consultas api servidor

---
+++ pages/api/categorias.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    const categorias = await prisma.categoria.findMany();
+    
+    res.status(200).json(categorias)
+    }
