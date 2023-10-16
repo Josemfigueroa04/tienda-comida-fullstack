1a7c4b70a4ec5291ba122193e4f4c3ade003b0f3
josemfiguero04 <josemfigueroa04@gmail.com>
2023-10-16 01:03:59 -0300

    add categorias al componente

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import { PrismaClient } from "@prisma/client"
 
-
-export default function Home() {
+export default function Home({ categorias}) {
+  console.log(categorias);
   return (
     <>
     <h1>Next Js</h1>
     
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prima = new PrismaClient();
+
+  const categorias = await prima.categoria.findMany();
+
+  return {
+    props: {
+      categorias,
+    }
+  }
 }
