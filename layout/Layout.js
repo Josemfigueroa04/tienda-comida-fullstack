ff56172e692c29c3791d749b78e1920970e84839
josemfiguero04 <josemfigueroa04@gmail.com>
2023-11-01 12:57:37 -0300

    add modal con producto

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,8 +1,27 @@
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
+import Modal from 'react-modal';
+import useQuiosco from '../hooks/useQuiosco';
+import ModalProducto from '../components/ModalProducto';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+Modal.setAppElement('#__next');
 
 
 export default function Layout({ children, pagina }) {
+
+    const { modal } = useQuiosco();
+
     return (
         <>
             <Head>
@@ -11,19 +30,22 @@ export default function Layout({ children, pagina }) {
             </Head>
 
             <div className='md:flex'>
-
                 <aside className='md:w-4/12 xl:w-1/4 2xl:w-1/5 '>
                     <Sidebar />
                 </aside>
 
                 <main className='md:w-8/12 xl: w-3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
                     <div className='p-10'>
-                     {children}   
+                        {children}
                     </div>
-                    
                 </main>
 
             </div>
+            {modal && (
+                <Modal isOpen={modal} style={customStyles}>
+                    <ModalProducto />
+                </Modal>
+            )}
 
         </>
 
